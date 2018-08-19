7e02135e0e2a134e012057f77ed607fc900bd4ab
Raghu Betina <raghu@firstdraft.com>
2018-08-19 15:48:34 +0000

    Indirect association between users and users

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,14 @@ class User < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :followers,
+             :through => :received_follow_requests,
+             :source => :sender
+
+  has_many   :following,
+             :through => :sent_follow_requests,
+             :source => :recipient
+
   # Validations
 
   # Include default devise modules. Others available are:
